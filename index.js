98a1fac67fcd9cbca042d783aa9ed6f0876b72af
ssmohanty153 <54876954+ssmohanty153@users.noreply.github.com>
2021-05-05 18:05:39 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,50 +1,19 @@
-const posts = [
-    {
-        titel: 'a', body: 'slim'
-    },
-    { titel: 'b', body: 'fat' }
-];
-function getposts() {
-    setTimeout(() => {
-        let output = '';
-        posts.forEach((data, index) => {
-            output += `<li>${data.titel}</li>`;
-        });
-       console.log(output);
-    }, 1000);
-}
+const io = require('socket.io')(3000);
+const users = {};
 
-function createpost(post, callback) {
-    setTimeout(() => {
-        posts.push(post);
-        callback();
+io.on('connection', socket => {
+    socket.on('new-user', name => {
+        console.log("username:"+name);
+      users[socket.id] = name;
+      socket.broadcast.emit('user-connected', name)
+    })
 
-    }, 2000);
-}
+    socket.on('onMessage', message => {
+        socket.broadcast.emit('recived', { message: message, name: users[socket.id] });
+    })
 
-createpost({ titel: 'c', body: 'skinny' }, getposts);
-//getposts();
-
-
-// function myDisplayer(some) {
-//     document.getElementById("demo").innerHTML = some;
-//   }
-
-//   function myCalculator(num1, num2) {
-//     let sum = num1 + num2;
-//     return sum;
-//   }
-
-//   let result = myCalculator(5, 5);
-//   myDisplayer(result);
-
-var person={name:"ssm",age:32,address:"abc",phone:{mobile:111,landline:091}};
-
-for(var key in person){
-    if(typeof(person[key])==='object'){
-        for(var data in person[key]){
-            console.log(person);
-        }
-    }
-    console.log(typeof(person[key]));
-}
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('sender', users[socket.id]);
+        delete users[socket.id];
+    })
+})
